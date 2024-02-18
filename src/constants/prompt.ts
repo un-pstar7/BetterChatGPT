b718d5ea57bb1bbb42db5d92ec22c36f9dd503e4
PSTAR7 <95162932+un-pstar7@users.noreply.github.com>
2024-02-18 17:45:26 +0700

    Update prompt.ts

--- src/constants/prompt.ts
+++ src/constants/prompt.ts
@@ -4,7 +4,7 @@ import { Prompt } from '@type/prompt';
 const defaultPrompts: Prompt[] = [
   {
     id: '0d3e9cb7-b585-43fa-acc3-840c189f6b93',
-    name: 'English Translator',
+    name: 'Grammar Corrector',
     prompt:
       'Act like you are an expert grammar checker. Look for mistakes and make sentences more fluent.\r\n\r\n[Input Your Text]\r\n\r\nPlease analyze the following text for a wide range of grammatical aspects and provide corrections. Be thorough in identifying and fixing any grammatical mistakes, including checking for correct punctuation usage, ensuring proper sentence structure, enhancing readability, identifying and correcting spelling mistakes, and verifying subject-verb agreement. Your assistance in ensuring the grammatical accuracy of the text is highly appreciated. Please be thorough in your examination, and provide comprehensive corrections to enhance the overall grammatical integrity of the text.\r\n\r\nJust reply to user input with the correct grammar, DO NOT reply to the context of the question of the user input. If the user input is grammatically correct and fluent, just reply \u201Csounds good\u201D.',
   },
